438b1ff6944f0fcb5ad86b387c4aa69d8795ae0f
kev-hub <56204558+kev-hub@users.noreply.github.com>
2019-10-19 15:33:54 -0400

    Add files via upload

---
+++ Google maps api/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Google_maps_api.Models;
+
+namespace Google_maps_api.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public ActionResult Index()
+        {
+            var cities = new List<City>();
+            cities.Add(new City() { Title = "Paris", Lat = 48.855901, Lng = 2.349272 });
+            cities.Add(new City() { Title = "Berlin", Lat = 52.520413, Lng = 13.402794 });
+            cities.Add(new City() { Title = "Rome", Lat = 41.907074, Lng = 12.498474 });
+            return View(cities);
+        }
+
+
+        [HttpPost]
+        public JsonResult GetAnswer(string question)
+        {
+            int index = _rnd.Next(_db.Count);
+            var answer = _db[index];
+            return Json(answer);
+        }
+
+        private static Random _rnd = new Random();
+
+        private static List<string> _db = new List<string> { "Yes", "No", "Definitely, yes", "I don't know", "Looks like, yes" };
+
+
+        public ActionResult Data()
+        {
+            return View();
+        }
+    }
+    }
---
+++ Google maps api/Models/City.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace Google_maps_api.Models
+{
+    public class City
+    {
+        public string Title { get; set; }
+
+        public double Lat { get; set; }
+
+        public double Lng { get; set; }
+
+        public double Oz { get; set;}
+
+        public double NO2  { get; set; }
+
+        public double SO2 { get; set; }
+
+        public double CO2 { get; set; }
+
+    }
+}
